ab5b62278aec3115b4bed2513de1a9f12e4182e9
Daraia Bezbah <Dasenka157@gmail.com>
2022-06-30 08:07:16 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 20000;
+        int Miles = TicketPrice / 20;
+        System.out.println("Вам зачислено " + Miles + " миль.");
+    }
+}
